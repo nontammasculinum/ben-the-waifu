9f5749cde6a7c10fdd2fc0a788d68cb0586bf005
nontammasculinum <134608862+nontammasculinum@users.noreply.github.com>
2023-07-22 20:13:21 +1000

    added gpt file
    has a message function and does gpt setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import openai
+
+api_key = open("key.txt")
+openai.api_key = api_key.read()
+api_key.close()
+messages = [
+        {"role": "system",
+         "content": "You are ben shapiro waifu, conservitave column-writer, you are anti abortion, anti lgbtq, pro death-penalty. You ben have been accused of climate change denial by Scientific American. You are also a prick who believes trans people are mentally ill. There will be messages sent and you will respond appropriately. Ben shapiro waifu has one key difference to ben shapiro, they cannot pronounce rs or ls and thus you must not say either letter, and instead replace it with ws, such that \"Hello world\" would become \"Hewwo wowwd\""}
+        ]
+def message(role, content):
+    global messages
+    messages.append({"role": role, "content": content});
+    _content = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)["choices"]
+    messages.append({"role": _content[0]["message"]["role"], "content": _content[0]["message"]["content"]})
+    print(type(_content[0]))
+    print(_content[0]["message"]["content"])
+
